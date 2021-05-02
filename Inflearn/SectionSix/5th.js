f750eefc8822650964e2e3a957b5e7772005c1cb
park-moen <pmj6516@naver.com>
2021-05-03 00:02:26 +0900

    스택, 큐 알고리즘

--- Inflearn/SectionSix/5th.js
+++ Inflearn/SectionSix/5th.js
@@ -1,6 +1,18 @@
 function solution(s) {
-  let answer;
+  let answer = 0;
+  let stack = [];
 
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') stack.push(s[i]);
+    else {
+      stack.pop();
+
+      if (s[i - 1] === '(') answer += stack.length;
+      else answer++;
+    }
+  }
+
+  console.log(stack);
   return answer;
 }
 
