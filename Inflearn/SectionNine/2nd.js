7a390f9088f57f53d841e6c0626e42fd64dd8000
park-moen <pmj6516@naver.com>
2021-05-12 23:27:25 +0900

    graph의 인접행렬 & 인접리스트 알고리즘

---
+++ Inflearn/SectionNine/2nd.js
@@ -0,0 +1,40 @@
+function solution(n, arr) {
+  let answer = 0;
+
+  const graph = Array.from(Array(n + 1), () => []);
+  const ch = Array.from({ length: n + 1 }, () => 0);
+
+  for (let [a, b] of arr) {
+    graph[a].push(b);
+  }
+
+  function DFS(v) {
+    if (v === n) answer++;
+    else {
+      for (let i = 0; i < graph[v].length; i++) {
+        if (ch[graph[v][i]] === 0) {
+          ch[graph[v][i]] = 1;
+          DFS(graph[v][i]);
+          ch[graph[v][i]] = 0;
+        }
+      }
+    }
+  }
+
+  ch[1] = 1;
+  DFS(1);
+  return answer;
+}
+
+let arr = [
+  [1, 2],
+  [1, 3],
+  [1, 4],
+  [2, 1],
+  [2, 3],
+  [2, 5],
+  [3, 4],
+  [4, 2],
+  [4, 5],
+];
+console.log(solution(5, arr));
