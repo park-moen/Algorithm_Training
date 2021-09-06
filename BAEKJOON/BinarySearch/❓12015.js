c452d3967f86839c42a4ef0842355e64d1148d57
park-moen <pmj6516@naver.com>
2021-09-06 12:53:41 +0900

    DynamicProgramming && BinarySearch

--- BAEKJOON/BinarySearch/❓12015.js
+++ BAEKJOON/BinarySearch/❓12015.js
@@ -1,4 +1,41 @@
 const fs = require('fs');
 const [n, ...arr] = fs.readFileSync('input.txt').toString().trim().split(/\s+/);
 
+const N = Number(n);
 const sequence = arr.map(num => Number(num));
+
+function lowerBound(target, dp) {
+  let answer = 0;
+  let sp = 0;
+  let ep = dp.length - 1;
+
+  while (ep >= sp) {
+    const mid = parseInt((sp + ep) / 2);
+    if (target <= dp[mid]) {
+      answer = mid;
+      ep = mid - 1;
+    } else {
+      sp = mid + 1;
+    }
+  }
+
+  return answer;
+}
+
+function solution(sequence) {
+  const dp = [];
+  dp.push(sequence[0]);
+
+  for (let item of sequence) {
+    if (item > dp[dp.length - 1]) {
+      dp.push(item);
+    } else {
+      const point = lowerBound(item, dp);
+      dp[point] = item;
+    }
+  }
+
+  console.log(dp.length);
+}
+
+solution(sequence, N);
--- BAEKJOON/DynamicProgramming/11053.js
+++ BAEKJOON/DynamicProgramming/11053.js
@@ -1,4 +1,28 @@
 const fs = require('fs');
 const [n, ...arr] = fs.readFileSync('input.txt').toString().trim().split(/\s+/);
 
+const N = Number(n);
 const sequence = arr.map(num => Number(num));
+
+function solution(sequence, len) {
+  let answer = 0;
+  const dp = Array.from({ length: len }, () => 0);
+  dp[0] = 1;
+
+  for (let i = 1; i < len; i++) {
+    let max = 0;
+
+    for (let j = i - 1; j >= 0; j--) {
+      if (sequence[i] > sequence[j] && max < dp[j]) {
+        max = dp[j];
+      }
+    }
+
+    dp[i] = max + 1;
+    answer = Math.max(answer, dp[i]);
+  }
+
+  console.log(answer);
+}
+
+solution(sequence, N);
