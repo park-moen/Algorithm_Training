e3d4aebb5f77d3a900127a1dc89607d3c0aa1983
park-moen <pmj6516@naver.com>
2021-10-31 17:50:23 +0900

    BOJ DP

---
+++ BAEKJOON/DynamicProgramming/9095.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('input.txt').toString().trim().split('\n');
+const t = Number(input.shift());
+
+for (let i = 0; i < t; i++) {
+  const n = Number(input.shift());
+}
