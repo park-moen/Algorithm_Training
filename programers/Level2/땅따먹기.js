2bee3d882925198110074e5d80cc06fd52ded449
park-moen <pmj6516@naver.com>
2022-09-02 21:03:37 +0900

    Daily Algorihtm: Programmers Level2 1문제 풀이 완료

--- programers/Level2/땅따먹기.js
+++ programers/Level2/땅따먹기.js
@@ -1,19 +1,16 @@
 function solution(land) {
-  let answer = Math.max(...land[0]);
-  let maxIndex = land[0].findIndex(value => value === answer);
+  let answer = [];
+  let len = land.length;
 
-  for (let i = 1; i < land.length; i++) {
-    land[i].splice(maxIndex, 1);
-
-    let maxValue = Math.max(...land[i]);
-
-    maxIndex = land[i].findIndex(value => value === maxValue);
-    answer += maxValue;
+  for (let i = 1; i < len; i++) {
+    land[i][0] += Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+    land[i][1] += Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+    land[i][2] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][3]);
+    land[i][3] += Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][2]);
   }
 
-  console.log(answer);
-
-  return answer;
+  answer = land[land.length - 1];
+  return Math.max(...answer);
 }
 
 const land = [
