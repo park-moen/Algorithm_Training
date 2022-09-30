fbb6ed5f9035540b79844acee944d4b2c39f9da6
park-moen <pmj6516@naver.com>
2022-09-30 14:50:26 +0900

    Algorithm_Training 폴더 정리 & ReadMe 파일 작성

--- Inflearn/SectionEight/13th.js
+++
@@ -1,7 +0,0 @@
-function solution(n, f) {
-  let answer;
-
-  return answer;
-}
-
-console.log(solution(4, 16));
--- Inflearn/SectionOne/3rd.js
+++
@@ -1,7 +0,0 @@
-function solution(n) {
-  let answer = Math.ceil(n / 12);
-
-  return answer;
-}
-
-console.log(solution(25));
