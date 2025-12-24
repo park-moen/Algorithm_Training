190d22fe161eaf751435f593e12250f256374884
park-moen <pmj6516@naver.com>
2025-12-24 19:34:08 +0900

    Algorithm 100 Day-Challenge: Day30
    - - 프로그래머스 Lv1:❕문자열 나누기

---
+++ programmers/level1/❕문자열 나누기.js
@@ -0,0 +1,18 @@
+function solution(s) {
+  let answer = 0;
+  let current;
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (count === 0) {
+      answer++;
+      current = s[i];
+      count = 1;
+    } else {
+      if (current !== s[i]) count--;
+      else count++;
+    }
+  }
+
+  return answer;
+}
